8d25234ba971ffaed36e9ac0afd0372e01582357
Uddipon <uddiponn@gmail.com>
2024-11-30 15:20:29 +0600

    Add project files.

---
+++ WebApii/Models/DbDoctor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApii.Models
+{
+    public class DbDoctorContext : DbContext
+       
+
+    {
+        public DbDoctorContext()
+        {
+
+        }
+        public DbDoctorContext(DbContextOptions<DbDoctorContext> options) : base(options)
+        {
+
+        }
+        
+        public DbSet<Doctor> Doctors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        {
+
+            optionsBuilder.UseSqlServer("DefaultConnection");
+
+        } 
+    }
+}
---
+++ WebApii/Models/Doctor.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApii.Models
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Specialty { get; set; }
+    }
+}
